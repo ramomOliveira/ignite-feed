647105628a56a6a314fdfc08f3a6f881723a197f
Ramome <ra-mom2000@hotmail.com>
2023-06-05 21:26:14 -0300

    feat:header created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import styles from "./Header.module.css";
 
+import igniteLogo from '../assets/ignite-logo.svg';
+
 export default function Header() {
   return (
     <header className={styles.header}>
-      <strong>Ignite Feed</strong>
+      <img src={igniteLogo} alt="Logotipo Ignite" />
+      <h1>Ignite Feed</h1>
     </header>
   );
 }
